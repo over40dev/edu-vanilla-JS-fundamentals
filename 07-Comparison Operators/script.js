d2cf10595dc8edf924f2f9e652c8c10dc25f63dd
over40dev <ted@over40.dev>
2021-01-14 08:48:10 -0330

    07-Comparison Operators

---
+++ 07-Comparison Operators/script.js
@@ -0,0 +1,44 @@
+let item = 'window';
+
+if(item == 'engine') {
+    console.log('engine!');
+}
+
+console.log('value of item is', item);
+
+let score = 5;
+
+if(score >= 60) {
+    console.log('pass');
+}
+else if (score < 10) {
+    console.log('meet the teacher');
+}
+else {
+    console.log('not pass');
+}
+
+let isEngine = item == 'engine';
+console.log(isEngine);
+
+if(isEngine) {
+
+}
+
+// // Challenge
+// let balance = 100;
+// let itemPrice = 1000;
+
+// // 1. check balance
+// // 2. if they buy, update balance
+
+// if(balance >= itemPrice) {
+//     // update balance
+//     //balance = balance - itemPrice;
+//     balance -= itemPrice;
+//     console.log('item purchased');
+//     console.log(balance);
+// }
+// else {
+//     console.log('not enough balance');
+// }
