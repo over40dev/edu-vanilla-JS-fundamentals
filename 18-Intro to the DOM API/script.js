d1900f8e91f4eee11ec40431547881d6c5826e39
over40dev <ted@over40.dev>
2021-01-26 22:43:41 -0330

    18-Intro to the DOM API

---
+++ 18-Intro to the DOM API/script.js
@@ -0,0 +1,17 @@
+// 1. Select
+let sign = document.getElementById('sign');
+// console.log(sign);
+
+// 2. Modify
+console.log('BEFORE:', sign.textContent);
+
+// modify text
+sign.textContent = 'Welcome, travelers!';
+
+console.log('AFTER:', sign.textContent);
+
+// modify HTML
+// sign.innerHTML = sign.innerHTML + '<p>hello</p>';
+
+// 3. change style
+sign.style.color = 'blue';
